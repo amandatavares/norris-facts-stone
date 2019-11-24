df3c1d1ddc2aa1e5f30408077913e720ac88b6e2
amandatvt <amandatvt@gmail.com>
2019-11-24 20:18:16 -0300

    Create Search ViewModel and Extensions folder

---
+++ NorrisFacts/ViewModel/SearchTableViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SearchTableViewModel.swift
+//  NorrisFacts
+//
+//  Created by Amanda Tavares on 24/11/19.
+//  Copyright © 2019 Amanda Tavares. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class SearchTableViewModel {
+    
+}
+
